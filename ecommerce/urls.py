2041a20d5932980b5818e12c059ae15ff65837a9
Mounir Bennacer <benphaoris@gmail.com>
2020-08-30 18:39:47 +0100

    adding link in the home.html and urls/views

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("shop", views.contact, name="shop_grid"),
+    path("details", views.contact, name="shop_details"),
+    path("cart", views.contact, name="cart"),
+    path("checkout", views.contact, name="checkout"),
+    path("blog", views.blog, name="blog"),
+    path("blog_details", views.contact, name="blog_details"),
+    path("contact", views.contact, name="contact"),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", {})
+
+
+def blog(request):
+    return render(request, "blog.html", {})
+
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+
+def details(request):
+    return render(request, "shop-details.html", {})
+
+
+def cart(request):
+    return render(request, "shoping-cart.html", {})
+
+
+def checkout(request):
+    return render(request, "checkout.html", {})
+
+
+def blog_details(request):
+    return render(request, "blog-details.html", {})
